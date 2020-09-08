1e01c6fb7d19a4669832efa871d8e41d1a062bb7
Sebastian Ertner <sebastian.ertner@netresearch.de>
2020-09-08 12:37:56 +0200

    GLS-17: create module skeleton

---
+++ registration.php
@@ -0,0 +1,10 @@
+<?php
+/**
+ * See LICENSE.md for license details.
+ */
+
+\Magento\Framework\Component\ComponentRegistrar::register(
+    \Magento\Framework\Component\ComponentRegistrar::MODULE,
+    'GlsGermany_Shipping',
+    __DIR__
+);
