1e2a92f895992b6da542fb18818adb3d7055f05c
Christoph Aßmann <christoph.assmann@netresearch.de>
2020-09-24 15:13:22 +0200

    GLS-25: update incoterm options in source model

--- Model/Config/Source/TermsOfTrade.php
+++ Model/Config/Source/TermsOfTrade.php
@@ -27,9 +27,11 @@ class TermsOfTrade implements OptionSourceInterface
     public function toArray(): array
     {
         return [
-            'incoterm1' => 'Incoterm Label 1',
-            'incoterm2' => 'Incoterm Label 2',
-            'incoterm3' => 'Incoterm Label 3',
+            '10' => __('DDP (Freight costs, customs costs & taxes paid)'),
+            '20' => __('DAP (Freight costs paid, customs costs & taxes unpaid)'),
+            '30' => __('DDP, VAT unpaid (Freight costs & customs costs paid, taxes unpaid)'),
+            '40' => __('DAP, cleared (Freight costs & customs clearance costs paid, customs duties and taxes unpaid)'),
+            '50' => __('DDP (Freight costs and customs clearance costs paid, lowvalue clearance)'),
         ];
     }
 }
